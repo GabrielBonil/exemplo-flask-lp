a11e9ea4a38cc5e943580675bab3022f1a8a7443
Gabriel Bonil da Silva <gabrielbonil32@gmail.com>
2021-11-15 22:00:52 +0000

    Metade do trabalho feito

--- app.py
+++ app.py
@@ -2,27 +2,72 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-tarefas = [
-    {"texto": "Estudar para a prova", "concluida": False},
-    {"texto": "Passear com o cachorro", "concluida": True},
-    {"texto": "Estudar mais para a prova", "concluida": False},
+pagamentos = [     
+    {"texto": "Pedro Henrique","forma": "Dinheiro", "status":"Pendente"},
+    {"texto": "Gabriel Bonil","forma": "Cartão", "status":"Pago" },
+    {"texto": "Robert John Downey, Jr", "forma": "Bitcoin", "status":"Pendente"},
+    {"texto": "Cris Evans", "forma": "Cheque" , "status":"Pago"},
+    {"texto": "Mark Ruffalo", "forma": "Pix", "status":"Pendente" },
+    {"texto": "Scarlett Johansson", "forma": "Boleto", "status":"Pago" },
+    {"texto": "Jeremy Renner", "forma": "Dinheiro", "status":"Pago" },
+    {"texto": "Chris Hemsworth", "forma": "Boleto", "status":"Pendente" },
 ]
 
 @app.route('/')
 def index():
-    return render_template('index.html', lista=tarefas)
+    return render_template('index.html', lista=pagamentos)
 
 @app.route('/create')
 def create():
     return render_template('create.html')
 
+@app.route('/delete')
+def delete():
+    return render_template('delete.html')
+
+@app.route('/pesquisa')
+def pesquisa():
+    return render_template('pesquisa.html')
+
+@app.route('/resultado')
+def resultado():
+    return render_template('resultado.html')
+
 @app.route('/save', methods=['POST'])  # <form action="/save" method="POST">
 def save():
-    texto = request.form['texto']      # <input name="texto"/>
-    tarefa = { "texto": texto, "concluida": False }
-    tarefas.append(tarefa)
+    nome = request.form['nome']      # <input name="nome"/>
+    forma = request.form['forma']      # <input name="forma"/>
+    status = request.form['status']      # <input name="status"/>
 
-    return redirect('https://5000-brown-lemur-kiu2j61j.ws-us18.gitpod.io/')
+    pagamento = { "texto": nome, "forma": forma, "status": status }
+    pagamentos.append(pagamento)
+
+    return redirect('https://5000-purple-boar-2j6fkdwx.ws-us18.gitpod.io/')
+
+@app.route('/del', methods=['POST'])  # <form action="/save" method="POST">
+def apagar():
+    nome = request.form['nome']      # <input name="nome"/>
+
+    a = 0
+    for i in pagamentos:
+        print(i["texto"])
+        if i["texto"] == nome:
+            del pagamentos[a]
+        a += 1
+
+    return redirect('https://5000-purple-boar-2j6fkdwx.ws-us18.gitpod.io/')
+
+@app.route('/pesquisa', methods=['POST'])  # <form action="/save" method="POST">
+def busca():
+    nome = request.form['nome']      # <input name="nome"/>
+    a = 0
+    resultado = []
+    for i in pagamentos:
+        if i["texto"] == nome:
+            resultado.append(pagamentos[a])
+        a += 1
+    
+    return redirect('https://5000-purple-boar-2j6fkdwx.ws-us18.gitpod.io/resultado')
 
 app.run(debug=True)
 
